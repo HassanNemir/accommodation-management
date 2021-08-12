8295428eee80627183571ea4adb2de78fea6b936
Hassan Nemir <eng-hassan.nemir1520@alexu.edu.eg>
2021-08-13 04:33:42 +0800

    rename forum files

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace accommodation_management
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new accommodationRequest());
         }
     }
 }
