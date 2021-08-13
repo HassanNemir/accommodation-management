bee37bff952f358505566ed22d1f4cbc0ba0e07d
Heaven <hwourache@gmail.com>
2021-08-13 22:55:02 +0800

    generate report update

---
+++ Check Room Availability.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace accommodation_management
+{
+    public partial class Check_Room_Availability : Form
+    {
+        public Check_Room_Availability()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            private string m_Room_Lists="test.xls"
+        }
+    }
+}
