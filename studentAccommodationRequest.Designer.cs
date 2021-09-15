4d8826077f4aa610a928e7980221dee3814975d4
Hassan Nemir <eng-hassan.nemir1520@alexu.edu.eg>
2021-09-16 07:53:55 +0800

    Remove student accommodation request redundant fields

--- studentAccommodationRequest.Designer.cs
+++ studentAccommodationRequest.Designer.cs
@@ -30,14 +30,8 @@ namespace accommodation_management
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.studentID = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
@@ -53,125 +47,71 @@ namespace accommodation_management
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(112, 760);
-            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Location = new System.Drawing.Point(84, 608);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 44);
+            this.button1.Size = new System.Drawing.Size(112, 35);
             this.button1.TabIndex = 0;
             this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(412, 135);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(238, 31);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(412, 213);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(238, 31);
-            this.textBox2.TabIndex = 2;
-            // 
             // studentID
             // 
-            this.studentID.Location = new System.Drawing.Point(412, 62);
-            this.studentID.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.studentID.Location = new System.Drawing.Point(309, 50);
+            this.studentID.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.studentID.Name = "studentID";
-            this.studentID.Size = new System.Drawing.Size(238, 31);
+            this.studentID.Size = new System.Drawing.Size(180, 26);
             this.studentID.TabIndex = 3;
             this.studentID.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(412, 298);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(238, 31);
-            this.textBox4.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(106, 67);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Location = new System.Drawing.Point(80, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(112, 25);
+            this.label1.Size = new System.Drawing.Size(87, 20);
             this.label1.TabIndex = 5;
             this.label1.Text = "Student ID";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(106, 140);
-            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(109, 25);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Full Name";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(106, 219);
-            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(150, 25);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Email Address";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(106, 304);
-            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(167, 25);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Contact Number";
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(106, 383);
-            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Location = new System.Drawing.Point(80, 128);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(108, 25);
+            this.label5.Size = new System.Drawing.Size(83, 20);
             this.label5.TabIndex = 9;
             this.label5.Text = "Start Date";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(106, 465);
-            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Location = new System.Drawing.Point(80, 205);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(101, 25);
+            this.label6.Size = new System.Drawing.Size(77, 20);
             this.label6.TabIndex = 12;
             this.label6.Text = "End Date";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(106, 544);
-            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Location = new System.Drawing.Point(80, 306);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(224, 25);
+            this.label7.Size = new System.Drawing.Size(167, 20);
             this.label7.TabIndex = 13;
             this.label7.Text = "Accommodation Block";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(274, 760);
-            this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button2.Location = new System.Drawing.Point(206, 608);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 44);
+            this.button2.Size = new System.Drawing.Size(112, 35);
             this.button2.TabIndex = 15;
             this.button2.Text = "Back";
             this.button2.UseVisualStyleBackColor = true;
@@ -183,20 +123,19 @@ namespace accommodation_management
             this.block.Items.AddRange(new object[] {
             "Block A",
             "Block B"});
-            this.block.Location = new System.Drawing.Point(412, 538);
-            this.block.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.block.Location = new System.Drawing.Point(309, 298);
+            this.block.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.block.Name = "block";
-            this.block.Size = new System.Drawing.Size(238, 33);
+            this.block.Size = new System.Drawing.Size(180, 28);
             this.block.TabIndex = 16;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(838, 62);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Location = new System.Drawing.Point(628, 50);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(94, 26);
+            this.label8.Size = new System.Drawing.Size(70, 20);
             this.label8.TabIndex = 17;
             this.label8.Text = "Block A";
             // 
@@ -204,40 +143,37 @@ namespace accommodation_management
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(838, 325);
-            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Location = new System.Drawing.Point(628, 260);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(94, 26);
+            this.label9.Size = new System.Drawing.Size(70, 20);
             this.label9.TabIndex = 18;
             this.label9.Text = "Block B";
             // 
             // startDate
             // 
             this.startDate.CustomFormat = "";
-            this.startDate.Location = new System.Drawing.Point(412, 375);
-            this.startDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.startDate.Location = new System.Drawing.Point(309, 122);
             this.startDate.Name = "startDate";
-            this.startDate.Size = new System.Drawing.Size(266, 31);
+            this.startDate.Size = new System.Drawing.Size(200, 26);
             this.startDate.TabIndex = 19;
             this.startDate.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // endDate
             // 
             this.endDate.CustomFormat = "\"MM DD YYYY\"";
-            this.endDate.Location = new System.Drawing.Point(412, 460);
-            this.endDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.endDate.Location = new System.Drawing.Point(309, 205);
             this.endDate.Name = "endDate";
-            this.endDate.Size = new System.Drawing.Size(266, 31);
+            this.endDate.Size = new System.Drawing.Size(200, 26);
             this.endDate.TabIndex = 20;
             // 
             // richTextBox1
             // 
             this.richTextBox1.BackColor = System.Drawing.SystemColors.MenuBar;
             this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox1.Location = new System.Drawing.Point(842, 135);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.richTextBox1.Location = new System.Drawing.Point(632, 108);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(482, 106);
+            this.richTextBox1.Size = new System.Drawing.Size(362, 85);
             this.richTextBox1.TabIndex = 21;
             this.richTextBox1.Text = "Block A has a swimming pool, as well as a lounge for the block tenants. ";
             // 
@@ -245,10 +181,10 @@ namespace accommodation_management
             // 
             this.richTextBox2.BackColor = System.Drawing.SystemColors.MenuBar;
             this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox2.Location = new System.Drawing.Point(842, 387);
-            this.richTextBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.richTextBox2.Location = new System.Drawing.Point(632, 310);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(482, 106);
+            this.richTextBox2.Size = new System.Drawing.Size(362, 85);
             this.richTextBox2.TabIndex = 22;
             this.richTextBox2.Text = "Block B has a gym, study rooms, and a laundry room. The gym and study rooms are o" +
     "nly for the block tenants, while the laundry room is for the students from both " +
@@ -256,9 +192,9 @@ namespace accommodation_management
             // 
             // studentAccommodationRequest
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.endDate);
@@ -270,16 +206,10 @@ namespace accommodation_management
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.studentID);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "studentAccommodationRequest";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "APU Accommodation | Student Accommodation Request";
@@ -291,14 +221,8 @@ namespace accommodation_management
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox studentID;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
