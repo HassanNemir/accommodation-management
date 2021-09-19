ed0c1efdecf531378c5e2bf624332cc27512c97b
Hassan Nemir <eng-hassan.nemir1520@alexu.edu.eg>
2021-09-19 23:08:01 +0800

    feedbk ui

--- wardenFeedback.Designer.cs
+++ wardenFeedback.Designer.cs
@@ -29,13 +29,78 @@ namespace accommodation_management
         /// </summary>
         private void InitializeComponent()
         {
+            this.studentIDList = new System.Windows.Forms.ComboBox();
+            this.studentName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // studentIDList
+            // 
+            this.studentIDList.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studentIDList.FormattingEnabled = true;
+            this.studentIDList.Location = new System.Drawing.Point(84, 51);
+            this.studentIDList.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.studentIDList.Name = "studentIDList";
+            this.studentIDList.Size = new System.Drawing.Size(908, 45);
+            this.studentIDList.TabIndex = 2;
+            this.studentIDList.Text = "List of Student ID";
+            // 
+            // studentName
+            // 
+            this.studentName.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.studentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studentName.Location = new System.Drawing.Point(32, 122);
+            this.studentName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.studentName.Name = "studentName";
+            this.studentName.Size = new System.Drawing.Size(421, 35);
+            this.studentName.TabIndex = 3;
+            this.studentName.Text = "Student Name";
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(480, 122);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(549, 35);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Student Name";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 191);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(421, 35);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Student Name";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(480, 191);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(549, 35);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Student Name";
             // 
             // wardenFeedback
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1064, 655);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.studentName);
+            this.Controls.Add(this.studentIDList);
             this.Name = "wardenFeedback";
             this.Text = "Warden | Review Feedback";
             this.ResumeLayout(false);
@@ -43,5 +108,11 @@ namespace accommodation_management
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox studentIDList;
+        private System.Windows.Forms.Label studentName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
