e7e4c3a72070281f2f5684c870b5ebf7bde7adfa
Hassan Nemir <eng-hassan.nemir1520@alexu.edu.eg>
2021-09-20 13:24:44 +0800

    fix program

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace accommodation_management
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Accommodation_Termination());
+            Application.Run(new Login());
         }
     }
 }
